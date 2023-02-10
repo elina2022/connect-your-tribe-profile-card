28411539619184d6cca4bc89ee9f5197e278ae09
Elina Kniazian <elinakniazian@gmail.com>
2023-02-10 11:16:07 +0100

    script import

---
+++ public/script.js
@@ -0,0 +1,27 @@
+function b1(){
+    document.getElementById("changingtext").innerHTML = "elinakniazian@gmail.com";
+}
+
+function b2(){
+    document.getElementById("changingtext").innerHTML = "06-11388334";
+}
+
+function b3(){
+    document.getElementById("changingtext").innerHTML = "Currently I like to...<br>dance<br>draw<br>read<br>bake";
+}
+
+function b4(){
+    document.getElementById("changingtext").innerHTML = "My qualities...<br>ambitious<br>(sometimes too) detail focused<br>creatief<br>some say stubborn, i say strong-willed ;)";
+}
+
+function b5(){
+    document.getElementById("changingtext").innerHTML = "Things I value...<br>alone time<br>health<br>learning new things<br>nature";
+}
+
+function b6(){
+    document.getElementById("changingtext").innerHTML = "Some things on my never ending bucketlist...<br> jump out of a plane (...with a parachute!)<br>finish a triathlon<br>stand under a waterfall<br>learn to speak Russian";
+}
+
+function b7(){
+    document.getElementById("changingtext").innerHTML = "18 years • Armenian ethnicity<br>1st year FDND<br>HTML, CSS and JavaScript ★☆☆☆☆<br><i>'watch a sunrise at least once a day'</i><br>Also...try to find the <u>hidden</u> button ;)";
+}
